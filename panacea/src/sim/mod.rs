91664a4613ff84dd231352c60858feb72a48b7e9
Calum Russell <calum.mj.russell@gmail.com>
2022-07-10 00:53:45 +0100

    fmt

--- panacea/src/sim/mod.rs
+++ panacea/src/sim/mod.rs
@@ -2,4 +2,4 @@ mod alator;
 mod antevorta;
 
 pub use self::alator::{alator_backtest, AlatorInput};
-pub use self::antevorta::{AntevortaBasicInput, antevorta_basic};
+pub use self::antevorta::{antevorta_basic, AntevortaBasicInput};
